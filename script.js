e760a4a9f9d68b903bbb834be4b958632a9fbc4b
EverythingsJake <136629909+EverythingsJake@users.noreply.github.com>
2023-10-20 21:51:08 -0400

    Add files via upload

--- script.js
+++ script.js
@@ -1,4 +1,21 @@
-// Sample script to add functionality
+// Previous script
 document.addEventListener("DOMContentLoaded", function() {
-  // Add some interactivity, if needed
+  // Classy feature: Click to make plants grow
+  document.querySelectorAll('.plant').forEach(function(plant) {
+    plant.addEventListener('click', function() {
+      if (plant.classList.contains('grow')) {
+        plant.classList.remove('grow');
+      } else {
+        plant.classList.add('grow');
+      }
+    });
+  });
+
+  // Position the plants randomly on the page
+  let plant1 = document.getElementById('plant1');
+  let plant2 = document.getElementById('plant2');
+  plant1.style.left = '10%';
+  plant1.style.top = '10%';
+  plant2.style.left = '80%';
+  plant2.style.top = '80%';
 });
